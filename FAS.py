d5a809071208fe53ad949dc0931609586c6ed11e
shubham kumar <92810522+Shubhamkumar90@users.noreply.github.com>
2023-03-05 12:01:10 +0530

    Update FAS.py

--- FAS.py
+++ FAS.py
@@ -9,25 +9,27 @@ from tkinter.messagebox import *
 import shutil
 import imghdr
 import re
+from PIL import ImageTk, Image
 
 PATH='images'
+
 class HomeWindow(Tk):
+
+
     def __init__(self):
         super().__init__()
         self.fm=FileManager()
         self.images=os.listdir(PATH)
-        self.title("Preview")
-        self.geometry("500x500")
-        # self.iconphoto(False,ph1)
-        self.configure(bg="skyblue")
-        Label(text="Welcome",font="bold 32",fg="red",bg="skyblue").pack(side=TOP)
-        # l2=Label(text="Enter some data",font="bold 18",fg="red")
-        # l2.pack(side=TOP)
-        Button(text="Start Attendence",font="bold 18",fg="red",bg="yellow",padx=20,pady=10,command=self.newwin).place(x=255,y=250)
-        Button(text="Add Image",font="bold 18",fg="brown",bg="yellow",padx=20,pady=10,command=self.selctFile).place(x=800,y=250)
-        Button(text="Show file",font="bold 18",bg="yellow",padx=20,pady=10,command=self.showFile).place(x=255,y=350)
-        Button(text="Remove Image",font="bold 18",fg="red",bg="yellow",padx=20,pady=10,command=self.remove_file).place(x=800,y=350)
-        Button(text="Quit",font="bold 18",fg="red",bg="yellow",padx=20,pady=10,command=quit).place(x=550,y=450)
+        self.title("FAS")
+        self.geometry("1050x620")
+        self.bg=ImageTk.PhotoImage(Image.open("Bgimage.png"))
+        Label(self,image=self.bg).place(x=0,y=0)#E7F6F2
+        Label(text="Facial Recognition Attendance System",font="Algerian 32",bg="White").place(x=200,y=10)
+        Button(text="Start Attendence",font="bold 18",bg="#F7F7F7",padx=20,pady=10,relief=RAISED,borderwidth=4,command=self.newwin).place(x=290,y=150)
+        Button(text="Add Image",font="bold 18",bg="#F7F7F7",padx=20,pady=10,relief=RAISED,borderwidth=4,command=self.selctFile).place(x=220,y=250)
+        Button(text="Show file",font="bold 18",bg="#F7F7F7",padx=30,pady=10,relief=RAISED,borderwidth=4,command=self.showFile).place(x=220,y=350)
+        Button(text="Remove Image",font="bold 18",bg="#F7F7F7",padx=20,pady=10,relief=RAISED,borderwidth=4,command=self.remove_file).place(x=270,y=450)
+        Button(text="Quit",font="bold 18",bg="#F7F7F7",padx=20,pady=10,relief=RAISED,borderwidth=4,command=quit).place(x=420,y=550)
         self.state('zoomed')
     
     
@@ -37,7 +39,7 @@ class HomeWindow(Tk):
             nw.StartWindow()
             nw.grab_set()
         else:
-            showinfo(title="Error",message="No Data Found!")
+            showwarning(title="Warning",message="No Data Found!")
 
 
     def selctFile(self):
@@ -51,7 +53,7 @@ class HomeWindow(Tk):
             sw.showWindow(self.fm.fileShow())
             sw.grab_set()
         else:
-            showinfo(message="No data found")
+            showinfo(title="Warning",message="No data found!")
     
     
     def remove_file(self):
@@ -59,28 +61,37 @@ class HomeWindow(Tk):
             self.fm.fileDeletion()
             self.images=os.listdir(PATH)
         else:
-            showinfo(title="Error",message="No Data Found!")
+            showwarning(title="Error",message="No Data Found!")
 
 
 
 class NewWindow(Toplevel):
+
+
     def __init__(self,parent):
         super().__init__(parent)
-        self.regex=re.compile('[.@_![#$%^&*()<>?/\|}{~:]')
+        self.regex=re.compile('[.,@_![#$%^&*()<>?/\|}{~:]')
+        self.number=['1','2','3','4','5','6','7','8','9','0']
         self.en=StringVar()
         self.box=Listbox(self)
         
     
     def detectface(self):
         if len(self.en.get().split()) != 0:
-            if not self.regex.search(self.en.get()) or not re.search('[]]', self.en.get()):
-                dect=Detection()
-                dect.FaceDetect(self.en.get())
-                self.en.set("")
+            if self.en.get()[0] not in self.number:
+                if not self.regex.search(self.en.get()) and not re.search('[]]', self.en.get()):
+                    dect=Detection()
+                    dect.FaceDetect(self.en.get())
+                    self.destroy()
+                    showinfo(title="Attendance Complete",message="Attendence completed successfully")
+                else:
+                    showerror("Error",message='Make sure subject name deos not contain any special character!')
+                    self.destroy()
             else:
-                showerror("Error",message='Make sure subject name deos not contain any special character!')
+                showerror("Warning","Subject name can not start with number!")
+                self.destroy()
         else:
-            showerror(title="Error",message="Enter Subject name")
+            showerror(title="Error",message="Please Enter Subject Name")
     
     
     def StartWindow(self):
@@ -104,45 +115,50 @@ class NewWindow(Toplevel):
         self.box.config(yscrollcommand= scrollbar.set)
         scrollbar.config(command= self.box.yview)
         self.box.bind("<Double 1>", self.curserSelection)
-            # self.reset
     
     
     def curserSelection(self,event):
         x = self.box.curselection()[0]
         file = self.box.get(x)
         f=FileManager()
-        f.stratFile(file)
+        f.startFile(file)
             
 
 
 class Detection():   # detection and attendance
+    
+    
     def __init__(self):
-        # self.en=name
-        # self.PATH='images'
         self.names=[]
         self.time=datetime.datetime.now()
-        self.encodedlist=self.encoding()
         self.cap = cv2.VideoCapture(0)
     
 
     def FaceDetect(self,en):
+        encodedlist=self.encoding()
         while(True):
         # Capture frame-by-frame
             ret, frame = self.cap.read()
-            faces=face_recognition.face_locations(frame)
-            encode_current_image=face_recognition.face_encodings(frame,faces)
+            faces=face_recognition.face_locations(cv2.resize(frame, (0,0), None, 0.5,0.5))  # model hog by default
+            encode_current_image=face_recognition.face_encodings(cv2.resize(frame, (0,0), None, 0.5,0.5),faces)
+            y=int(len(faces)/4*10)
             for encoded_face,faceL in zip(encode_current_image,faces):
                 top, right, bottom, left = faceL
+                top*=2 
+                right*=2
+                bottom*=2
+                left*=2 
                 cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
                 try:
-                    matchface=face_recognition.compare_faces(self.encodedlist, encoded_face)
-                    result=face_recognition.face_distance(self.encodedlist, encoded_face)
+                    matchface=face_recognition.compare_faces(encodedlist, encoded_face)
+                    result=face_recognition.face_distance(encodedlist, encoded_face)
                     index=np.argmin(result)
                     if matchface[index]:
                         name=self.names[index].upper()
                         self.takeAtttendance(name,en)
                         cv2.putText(frame,f"{name}",(left+5,top),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255),1)
-                        cv2.putText(frame,f"{name}'s attendance marked",(200,50),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255),1)
+                        cv2.putText(frame,f"{name}'s attendance marked",(200,50+y),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255),1)
+                        y-=15
                     else:
                         cv2.putText(frame,"UNKNOWN",(left,bottom),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),1)
                 # cv2.rectangle(frame, (left, bottom-35), (right, bottom), (0, 255, 0), cv2.FILLED)
@@ -152,7 +168,6 @@ class Detection():   # detection and attendance
             cv2.putText(frame,"Press Q to quit",(10,450),cv2.FONT_HERSHEY_COMPLEX,0.7,(255,0,0),2)
             # Display the resulting frame
             cv2.imshow('Attendence', frame)
-            # cv2.waitKey(1)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
         self.cap.release()
@@ -163,23 +178,27 @@ class Detection():   # detection and attendance
         ername=""
         encode=[]     
         imageList=[]
+        ext=[]
         self.names.clear()
         images=os.listdir(PATH)
         for cl in images:
             pimg=cv2.imread(f"{PATH}/{cl}")
             imageList.append(pimg)
             self.names.append(os.path.splitext(cl)[0])
-        for img,nm in zip(imageList,self.names):
+            ext.append(os.path.splitext(cl)[1])
+        for img,nm,ex in zip(imageList,self.names,ext):
             try:
                 image=cv2.resize(img, (0,0), None, 0.25,0.25)
                 encoded_face = face_recognition.face_encodings(image)[0]
                 encode.append(encoded_face)
             except:
-                # print(nm)
-                ername=ername+" "+nm
+                ername=ername+" "+nm+ex
         if ername:
-            showwarning(message=f"{ername} is not clear or no face detected")
-        # print(names)
+            ename=ername.split(" ")[1:]
+            for n in ename:
+                os.remove(f"{PATH}/{n}")
+            showerror(title="Error",message=f"{ername.replace(' ', ',')[1:]} is not clear or no face detected")
+            return False
         return encode
 
 
@@ -194,10 +213,10 @@ class Detection():   # detection and attendance
                 f.write("Name,Time")
                 f.write(f"\n{name},{self.time.hour}:{self.time.minute}")
         
-
+ 
 class FileManager():
     def __init__(self):
-        self.file=(('jpg files','*.jpg*'),('All files', '*.*'))
+        self.file=(('jpg files','*.jpg*'),('png files', '*.png'),('All files','*.*'))
         self.name=""
         self.f=Detection()
         
@@ -211,14 +230,13 @@ class FileManager():
                     if not os.path.exists(PATH+"\\"+os.path.basename(filename)):
                         shutil.copy(filename,PATH)
                         self.name=self.name+os.path.basename(filename)+","
-                        # showinfo(message=f"{os.PATH.basename(filename)} added")
                     else:
-                        showinfo(message="File already exist")
+                        showinfo(message="File already exist",title="Existing Data")
                 else:
-                    showwarning(message="Please select images")
+                    showwarning(title="Warning",message="Please select images")
             if self.name:
-                self.f.encoding()
-                showinfo(message=f"{self.name[:-1]} added")
+                if self.f.encoding():
+                    showinfo(title="Image Addition",message=f"{self.name[:-1]} added")
     
 
     def fileShow(self):
@@ -226,29 +244,27 @@ class FileManager():
         return files
 
 
-    def stratFile(self,file):
+    def startFile(self,file):
         os.startfile(file)
 
 
     def fileDeletion(self):
         name=""
-        filet=(('jpg files','*.jpg*'),('All files', '*.*'))
-        filenames=filedialog.askopenfilenames(title='Open files',initialdir=PATH,filetypes=filet)
+        filenames=filedialog.askopenfilenames(title='Open files',initialdir=PATH,filetypes=self.file)
         if filenames:
             for filename in filenames:
                 if os.getcwd()+"\\"+PATH == os.path.dirname(filename).replace("/","\\"):
                     os.remove(filename)
                     name+=os.path.basename(filename)+","
-                    # showinfo(message=f"{os.path.basename(filename)} deleted")
                 else:
                     showerror(message="Please select image from the current folder",title="Out of range")
             if name:
-                showinfo(message=f"{name[:-1]} deleted")
+                showinfo(message=f"{name[:-1]} deleted",title="Image Deletion")
         
 
 
 if __name__=="__main__":
     if PATH not in os.listdir():
         os.mkdir(PATH)
-    b=HomeWindow()
-    b.mainloop()
+    hw=HomeWindow()
+    hw.mainloop()
